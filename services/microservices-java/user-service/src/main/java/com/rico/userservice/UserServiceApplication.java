719b1be5148baea163023161b8165a884087c437
RicoLancheros <rico.lancheros@hotmail.com>
2025-02-15 23:54:10 -0500

    V0.0.1.6: No me esta dando la conexion a la base de datos en docker, no se donde esta el problema ya en este punto :( igual

--- services/microservices-java/user-service/src/main/java/com/rico/userservice/UserServiceApplication.java
+++ services/microservices-java/user-service/src/main/java/com/rico/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
+	//La aplicacion se inicia con el metodo main en donde se ejecuta el metodo run de SpringApplication
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
